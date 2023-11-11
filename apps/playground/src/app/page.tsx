9fc7120580cc5ddcfe4e02a1d23e23bd0da59485
Bruno Barbosa <brunoib@domtec.com.br>
2023-11-11 13:54:36 -0300

    :recycle: refactor: Reorganização do playground e Next.js 13

--- apps/playground/src/app/page.tsx
+++ apps/playground/src/app/page.tsx
@@ -1,21 +1,11 @@
-"use client";
-
-import { Button, ThemeProvider, createTheme } from "bib-ui";
-
-const myTheme = createTheme({
-  colors: {
-    violet: "#11c290",
-  },
-});
+import { Button } from "bib-ui";
 
 export default function Home() {
   return (
-    <ThemeProvider theme={myTheme}>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Button color="violet" variant="default">
-          TESTE
-        </Button>
-      </main>
-    </ThemeProvider>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button color="orange" variant="default">
+        TESTE
+      </Button>
+    </main>
   );
 }
